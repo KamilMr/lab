4abedc6b664d455ff1755acb32f03f8cb092e0c8
kamil <kamil@chicos.life>
2025-04-15 13:19:05 +0200

    Test errors

--- nodejs/basic/app/basic-express.js
+++ nodejs/basic/app/basic-express.js
@@ -3,11 +3,27 @@ const express = require("express");
 const app = express();
 
 app.use("/mid", (req, res) => {
+  throw new Error("Error from mid");
   res.send("<h1>Hello there from mid</h1>");
 });
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello there</h1>");
+  res.send("<button onclick='window.location.href=\"/mid\"'>Test</button>");
 });
 
-app.listen(3001, () => console.log("server is running on port 3001"));
+app.use("/*slate", (req, res) => {
+  res.redirect("/");
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send("Something broke!");
+});
+
+app.listen(3001, (err) => {
+  if (err) {
+    console.log('error happended');
+  } else {
+    console.log("server is running on port 3001");
+  }
+});
