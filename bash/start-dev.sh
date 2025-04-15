62cdd09632e511416f399d6902f25605cb6eac1e
kamil <kamil@chicos.life>
2025-04-15 13:18:13 +0200

    Refactor start-dev.sh

--- bash/start-dev.sh
+++ bash/start-dev.sh
@@ -1,34 +1,24 @@
 #!/bin/bash
-# Simple basic script that opens multiple windows in tmux simultaneously, each connected to a different repository.
 
-# Define session name
-SESSION="skb-all"
+# Define directories and their respective commands
+directories=("b" "f")
+commands=("npm run dev" "npm run start")
 
-# Define arrays for window names, directories, and commands
-WINDOW_NAMES=("sso-b" "sso-fe" "cv-be" "inter")
-DIRS=("sso_be" "sso_f" "cv-be" "inter")
-COMMANDS=("npm run dev" "npm run dev" "npm run dev" "npm run start")
+tmux kill-server # Kill any existing tmux server
 
-# Kill existing session with the same name
-tmux kill-session -t $SESSION
+for i in "${!directories[@]}"; do
+  d="${directories[$i]}"
+  cmd="${commands[$i]}"
+  echo "$d"
+  cd "$d" && { 
+    session_name="${d//\//_}" # Replace '/' with '_' for tmux session name
 
-# Start a new tmux session for the first window
-tmux new-session -d -s $SESSION -n ${WINDOW_NAMES[0]}
-tmux split-window -h
-tmux send-keys "cd ${DIRS[0]} && v ." C-m
-tmux select-pane -t 0
-tmux send-keys "cd ${DIRS[0]} && ${COMMANDS[0]}" C-m
-tmux resize-pane -L 85
+    if ! tmux has-session -t "$session_name" 2>/dev/null; then
+      tmux new-session -d -s "$session_name"
+    fi
+    tmux send-keys -t "$session_name" "$cmd" C-m
 
-# Loop through the rest of the windows
-for i in {1..3}; do
-    tmux new-window -t $SESSION -n ${WINDOW_NAMES[$i]}
-    tmux split-window -h
-    tmux send-keys "cd ${DIRS[$i]} && v ." C-m
-    tmux select-pane -t 0
-    tmux send-keys "cd ${DIRS[$i]} && ${COMMANDS[$i]}" C-m
-    tmux resize-pane -L 75
+    cd - > /dev/null # Go back to the previous directory
+  }
 done
 
-# Attach to the tmux session
-tmux attach-session -t $SESSION
