2d5ffc8e5ba75ff93ff966871caf837c8e3b1d1c
kamil <kamil@chicos.life>
2025-06-01 17:55:20 +0200

    Add .gitignore and implement publish script for processing files with image attachments

---
+++ nodejs/blog-script/publish.js
@@ -0,0 +1,99 @@
+const fs = require("node:fs");
+const path = require("node:path");
+const { Transform } = require("node:stream");
+const { pipeline } = require("node:stream");
+
+const IMAGE_ATTACHMENT_PATH = "attachments";
+
+const assertExists = (p) => {
+  const abs = resolveRelative(p);
+  if (!fs.existsSync(abs)) throw new Error(`${abs} does not exist`);
+};
+
+const resolveRelative = (p) => {
+  if (path.isAbsolute(p)) return p;
+
+  return path.resolve(process.cwd(), p);
+};
+
+function doReplace(full, inner) {
+  const imagePath = path.join(this.source, IMAGE_ATTACHMENT_PATH, inner);
+  if (!fs.existsSync(resolveRelative(imagePath))) return full;
+
+  const { ext, name } = path.parse(inner);
+  const safe = name.toLowerCase().replace(/[\s.]/g, "-");
+  const newName = safe + ext;
+  fs.copyFileSync(
+    resolveRelative(imagePath),
+    path.join(this.imageDirectory, newName)
+  );
+  return `![${safe}](/images/${newName})`;
+}
+
+class CustomTransformStream extends Transform {
+  constructor({ searchPattern, source, target, options, imageDirectory }) {
+    super({ ...options, encoding: "utf-8", objectMode: true });
+    this.searchPattern = searchPattern;
+    this.tail = "";
+    this.source = source;
+    this.target = target;
+    this.imageDirectory = imageDirectory;
+  }
+
+  _transform(chunk, encoding, cb) {
+    const overlap = 64;
+    const str = this.tail + chunk;
+    const replaced = str.replace(this.searchPattern, doReplace.bind(this));
+    this.tail = replaced.slice(-overlap);
+    this.push(replaced.slice(0, -overlap));
+    cb();
+  }
+
+  _flush(cb) {
+    this.push(this.tail);
+    cb();
+  }
+}
+
+const iterateOverAll = (source, target, imageDirectory) => {
+  assertExists(source);
+  assertExists(target);
+  assertExists(imageDirectory);
+
+  const files = fs.readdirSync(source);
+
+  for (const file of files) {
+    if (fs.statSync(path.join(source, file)).isDirectory()) continue;
+    const fileStream = fs.createReadStream(path.join(source, file), {
+      encoding: "utf-8",
+    });
+    const writeStream = fs.createWriteStream(path.join(target, file));
+
+    const transformStream = new CustomTransformStream({
+      searchPattern: /!\[\[(.*?)\]\]/g,
+      source,
+      target,
+      imageDirectory,
+    });
+
+    pipeline(fileStream, transformStream, writeStream, (err) => {
+      if (err) {
+        console.error(`There is an error: ${err}`);
+        process.exit(1);
+      }
+    });
+  }
+};
+
+const args = process.argv.slice(2);
+
+if (args.length !== 3) {
+  console.error("Usage: node publish.js <sourceDir> <targetDir> <imageDir>");
+  process.exit(1);
+}
+
+const sourceDirectory = args[0];
+const targetDirectory = args[1];
+const imageDirectory = args[2];
+
+iterateOverAll(sourceDirectory, targetDirectory, imageDirectory);
