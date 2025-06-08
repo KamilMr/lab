bb46b6f2336625990f9cc25ba7bfbbe5f796b547
kamil <kamil@chicos.life>
2025-06-08 15:37:48 +0200

    pluckByKeys function for data extraction

--- nodejs/various.js
+++ nodejs/various.js
@@ -1,4 +1,4 @@
-/*Basic checking if file is pdf*/ 
+/*Basic checking if file is pdf*/
 const handleUploadStatement = async (req, res, next) => {
 
   if (req.headers['content-type'] !== 'application/pdf')
@@ -10,4 +10,19 @@ const handleUploadStatement = async (req, res, next) => {
   if (fileSignature !== '%PDF-') return next('not_pdf');
 
   res.ch.ok();
+};
+
+const pluckByKeys = (data = {}, keys = []) => {
+  // validate keys, has to be an array or string
+  if (!Array.isArray(keys) && typeof keys !== 'string') return data;
+
+  // also when no keys are provided skip
+  if (!keys.length) return data;
+  const dict = _.keys(data);
+
+  const requested = Array.isArray(keys) ? keys : keys.toString().split(',');
+  return requested.reduce((acc, v) => {
+    if (dict.includes(v)) acc[v] = data[v];
+    return acc;
+  }, {});
 };
