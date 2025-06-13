19f94cd62c2400fb75a94cb1a0f5d89b12dd154d
kamil <kamil@chicos.life>
2025-06-13 18:06:17 +0200

    Add timer demonstration in 1-timer.js to illustrate callback delays

---
+++ nodejs/event-loop/1-timer.js
@@ -0,0 +1,13 @@
+// helper function to log the timer callback with the delay
+const timerCallback = (a, b) => console.log(`Timer callback ${a} delayed by ${Date.now() - start - b}`);
+
+// get the start time
+const start = Date.now();
+
+// Interesting to see how timers are executed and delayed.
+
+// set the timers
+setTimeout(timerCallback, 100, "100 ms", 100);
+setTimeout(timerCallback, 0, "0 ms", 0);
+setTimeout(timerCallback, 1, "1 ms", 1);
+setTimeout(timerCallback, 300, "300 ms", 300);
