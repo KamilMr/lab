176e82d38430b68bfb13ae797031bf02040958dc
kamil <kamil.mrowka@outlook.com.au>
2025-06-21 14:34:27 +0200

    Remove useless script

--- nodejs/strace-testing-node/install.sh
+++ nodejs/strace-testing-node/install.sh
@@ -7,9 +7,4 @@ wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"; [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install 24 && node -v
-
-# Install Node.js 24
-nvm install 24
-
-# Set Node.js 24 as default
 nvm alias default 24
