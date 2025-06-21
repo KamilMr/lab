2e973817a5a92b658c7b35f111258696285d7cdd
kamil <kamil.mrowka@outlook.com.au>
2025-06-21 14:29:54 +0200

    Add install script

---
+++ nodejs/strace-testing-node/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt update && apt install -y wget vim strace
+
+# Install Node.js
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"; [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install 24 && node -v
+
+# Install Node.js 24
+nvm install 24
+
+# Set Node.js 24 as default
+nvm alias default 24
+
+# Verify installation
