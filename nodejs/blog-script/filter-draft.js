751715d150a8120b251821c933012801734fe560
kamil <kamil.mrowka@outlook.com.au>
2025-07-03 09:07:15 +0200

    Push smaller chunks down the stream

--- nodejs/blog-script/filter-draft.js
+++ nodejs/blog-script/filter-draft.js
@@ -12,7 +12,14 @@ class FilterStream extends Transform{
   }
   _flush(cb) {
     const isThere = searchString.test(this.tail);
-    if (!isThere) return this.push(this.tail);
+    if (!isThere) {
+      // iterate over lines and push them to the stream
+      const lines = this.tail.split('\n');
+      lines.forEach((line, index) => {
+        if (index + 1 === lines.length) return this.push(line);
+        this.push(line + '\n');
+      });
+    }
     this.tail = '';
     cb();
   }
