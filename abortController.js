86642bd0d8bb87445e659aaea469f532f83e04e1
kamil <kamil.mrowka@outlook.com.au>
2025-07-16 16:40:39 +0200

    Rm indent

--- abortController.js
+++ abortController.js
@@ -1,10 +1,10 @@
-    const controller = new AbortController();
+const controller = new AbortController();
 
-    fetch('https://api.example.com/data', { signal: controller.signal })
-      .then(response => response.json())
-      .then(data => setData(data))
-      .catch(error => {
-        if (error.name !== 'AbortError') {
-          console.error('Fetch error:', error);
-        }
-      });
+fetch('https://api.example.com/data', { signal: controller.signal })
+  .then(response => response.json())
+  .then(data => setData(data))
+  .catch(error => {
+    if (error.name !== 'AbortError') {
+      console.error('Fetch error:', error);
+    }
+  });
