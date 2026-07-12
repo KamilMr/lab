9f7aaf823d7553584532d943262f478cd3c50723
kamil <kamil.mrowka@outlook.com.au>
2026-07-12 13:58:35 +0200

    Add missing notes

---
+++ bash/sepcial-characters.sh
@@ -0,0 +1,19 @@
+# "a meaning beyond its literal meaning"
+#
+
+# Comment
+echo "The # here does not begin a comment."
+echo 'The # here does not begin a comment.'
+echo The \# here does not begin a comment.
+echo The # here begins a comment.
+
+echo ${PATH#*:}       # Parameter substitution, not a comment.
+echo $(( 2#101011 ))  # Base conversion, not a comment.
+
+# Terminates in case of `;;`
+# dot
+# Before file is hidden file and `fs` will not show.
+# When matching `.` match single character.
+# quotes
+# `"` preserves most of special characters withing string
+# `'` preserves all of special characters withing string
